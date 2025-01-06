d2a2a7b0c1c87f099f947c19a64f2164539ec1bb
Ricardo Lacerda <revricardolacerda@gmail.com>
2025-01-06 06:04:22 -0300

    inicia projeto

---
+++ src/main/java/br/com/petz/clientepet/ClientepetApplication.java
@@ -0,0 +1,13 @@
+package br.com.petz.clientepet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientepetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientepetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/petz/clientepet/ClientepetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petz.clientepet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientepetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
