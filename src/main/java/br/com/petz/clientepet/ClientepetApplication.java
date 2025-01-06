562b8276df42fa3a3f025daaaf99e97bacaefec4
Ricardo Lacerda <revricardolacerda@gmail.com>
2025-01-06 06:31:35 -0300

    Implementa teste aplicação web

--- src/main/java/br/com/petz/clientepet/ClientepetApplication.java
+++ src/main/java/br/com/petz/clientepet/ClientepetApplication.java
@@ -2,10 +2,18 @@ package br.com.petz.clientepet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientepetApplication {
-
+	@GetMapping
+	public String getHomeTeste() {
+		return "Cadastro de Clientes";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ClientepetApplication.class, args);
 	}
