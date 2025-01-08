039e4da365aef5a2ae0bac82575f9d6cee7ef3df
Ricardo Lacerda <revricardolacerda@gmail.com>
2025-01-08 04:27:56 -0300

    Implementa Controller

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -1,10 +1,16 @@
 package br.com.petz.clientepet.cliente.application.api;
 
-public class ClienteController implements ClienteAPI {
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@Log4j2
+public class ClienteController implements ClienteAPI {
 	@Override
 	public ClienteResponse postCliente(ClienteRequest clienteRequest) {
-		// TODO Auto-generated method stub
+		log.info("[inicia] ClienteController --> postCliente");
+		log.info("[finaliza] ClienteController --> postCliente");
 		return null;
 	}
 
