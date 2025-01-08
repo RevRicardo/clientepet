35f4cc93f0b3432676582b6cfb934503bbe47a57
Ricardo Lacerda <revricardolacerda@gmail.com>
2025-01-08 04:15:10 -0300

    Implementa contrato API

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/cliente")
 public interface ClienteAPI {
 	@PostMapping
-	@ResponseStatus(code = HttpStatus.CREATED);
-	//recebe uma Request de Cliente e retorna uma Response de Cliente. 
+	@ResponseStatus(code = HttpStatus.CREATED) 
 	ClienteResponse postCliente(@RequestBody ClienteRequest clienteRequest);
 }
---
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -0,0 +1,11 @@
+package br.com.petz.clientepet.cliente.application.api;
+
+public class ClienteController implements ClienteAPI {
+
+	@Override
+	public ClienteResponse postCliente(ClienteRequest clienteRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
